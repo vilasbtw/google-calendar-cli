93d93d43f2cae8539ce393533aede70d487c32ad
vilasbtw <kaiquevilas@icloud.com>
2025-07-17 16:22:33 -0300

    Implementing List and Insert agendas functions

--- cmd/events/events.go
+++ cmd/events/events.go
@@ -2,8 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/spf13/cobra"
+	"github.com/vilasbtw/google-calendar-cli/internal/calendar"
 )
 
 var EventsCmd = &cobra.Command{
@@ -11,6 +13,13 @@ var EventsCmd = &cobra.Command{
 	Short: "Check events",
 	Long:  "Find all your calendar events",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Choose the command: today or week")
+		c := calendar.NewClient()
+		id, err := c.GetAgendaID()
+
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		fmt.Println("id:", id)
 	},
 }
--- internal/calendar/calendar.go
+++ internal/calendar/calendar.go
@@ -4,11 +4,19 @@ package calendar
 
 import (
 	"context"
+	"errors"
 	"log"
 	"os"
 
 	gCalendar "google.golang.org/api/calendar/v3"
 	"google.golang.org/api/option"
+)
+
+const AGENDA = "Roles"
+
+var (
+	ErrAgendaNotFound       = errors.New("unable to find the agenda")
+	ErrUnableToInsertAgenda = errors.New("unable to insert the agenda")
 )
 
 type Calendar struct {
@@ -20,15 +28,44 @@ func NewClient() *Calendar {
 	credentials, err := os.ReadFile("./credentials.json")
 
 	if err != nil {
-		log.Fatal("Unable to read credentials.json")
+		log.Fatal("unable to read credentials.json")
 	}
 
 	service, err := gCalendar.NewService(ctx, option.WithCredentialsJSON(credentials))
 	if err != nil {
-		log.Fatal("Unable to create Google Calendar Service: %s", err.Error())
+		log.Fatalf("unable to create Google Calendar Service: %s", err.Error())
 	}
 
 	return &Calendar{
 		Service: service,
 	}
+}
+
+func (c *Calendar) GetAgendaID() (string, error) {
+	// GET request for agenda "Roles"
+	list, err := c.Service.CalendarList.List().Do()
+	if err != nil {
+		log.Fatal("unable to list agendas.")
+	}
+
+	for _, v := range list.Items {
+		if v.Summary == AGENDA {
+			return v.Id, nil
+		}
+	}
+	return "", ErrAgendaNotFound
+}
+
+func (c *Calendar) InsertAgenda(id string) error {
+
+	entry := &gCalendar.CalendarListEntry{
+		Id: id,
+	}
+
+	_, err := c.Service.CalendarList.Insert(entry).Do()
+	if err != nil {
+		return ErrUnableToInsertAgenda
+	}
+
+	return nil
 }
